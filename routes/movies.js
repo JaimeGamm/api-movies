454deb0f73b88b4e65d9865f96cbb1aeb2cc0dfc
jaimegamm <jaimegamm@gmail.com>
2024-06-29 21:57:34 -0500

    version 4

--- routes/movies.js
+++ routes/movies.js
@@ -1,56 +1,15 @@
 import { Router } from "express";
 
-import { validateMovie, validatePartialMovie } from "../schemas/movies.js";
-
-import { MovieModel } from "../model/movie.js";
+import { MovieContoller } from "../controllers/movies.js";
 
 export const moviesRouter = Router();
 
-moviesRouter.get("/", async (req, res) => {
-  const { genre } = req.query;
-  const movies = await MovieModel.getAll({ genre });
-  res.json(movies);
-});
+moviesRouter.get("/", MovieContoller.getAll);
 
-moviesRouter.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const movie = await MovieModel.getById({ id });
-  if (movie) return res.json(movie);
-  res.status(404).json({ message: "Movie not found" });
-});
+moviesRouter.get("/:id", MovieContoller.getById);
 
-moviesRouter.post("/", async (req, res) => {
-  const result = validateMovie(req.body);
+moviesRouter.post("/", MovieContoller.create);
 
-  if (!result.success) {
-    // 422 Unprocessable Entity
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-  const newMovie = await MovieModel.create({ input: result.data });
+moviesRouter.delete("/:id", MovieContoller.delete);
 
-  res.status(201).json(newMovie);
-});
-
-moviesRouter.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const result = await MovieModel.delete({ id });
-
-  if (result === false) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  return res.json({ message: "Movie deleted" });
-});
-
-moviesRouter.patch("/:id", async (req, res) => {
-  const result = validatePartialMovie(req.body);
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const { id } = req.params;
-  const updateMovie = await MovieModel.update({ id, input: result.data });
-  return res.json(updateMovie);
-});
+moviesRouter.patch("/:id", MovieContoller.update);
